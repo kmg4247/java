27ab7a9085f7e784f7d031ac116fd57367d7443e
MinGyu <MinGyu@DESKTOP-IOOQ2FU>
2019-06-04 17:04:33 +0900

    4번째 Commit

---
+++ standard_of_java/src/ch02/SpecialCharEx.java
@@ -0,0 +1,15 @@
+package ch02;
+
+public class SpecialCharEx {
+
+	public static void main(String[] args) {
+		
+		System.out.println('\''); // 작은 따옴표, '''처럼 할 수 없다.
+		System.out.println("abc\t123\b456"); // \b에 의해 3이 지워진다.
+		System.out.println("\n"); // 개행 문자 출력하고 개행
+		System.out.println("\"HELLO\""); // 큰 따옴표를 출력하려면 이렇게 한다.
+		System.out.println("c:\\"); 
+	
+	}
+
+}
